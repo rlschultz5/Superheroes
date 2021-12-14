6e2864916686c7cd3d008f979a73760fdff3a84d
Rob Schultz <rlschultz5@wisc.edu>
2021-12-13 19:12:47 -0600

    Icon added to browser

---
+++ src/app/superher/superhero.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { SuperheroesService } from '../services/superheroes.service';
+
+@Component({
+  selector: 'superheroes',
+  template: `
+    <h2>{{ title }}</h2>
+    <ul>
+      <li *ngFor="let superhero of superheroes">
+        {{ superhero }}
+      </li>
+    </ul>    
+    `
+})
+export class SuperheroComponent implements OnInit {
+  title = "List of Superheroes:";
+  superheroes;
+  // constructor() {
+  //   let service = new SuperheroesService();
+  //   this.superheroes = service.getSuperheroes();
+  // }
+  // Auto-constructs an instance and passes it in
+  constructor(service: SuperheroesService) {
+    this.superheroes = service.getSuperheroes();
+  }
+  ngOnInit(): void {
+  }
+
+}
